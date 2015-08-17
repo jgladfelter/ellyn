9100e3096cb44c4f4e0abfcb662200ca9931a36a
mknSL2015 <maggieknorris@gmail.com>
2015-08-17 11:31:07 -0500

    added to database

--- app/admin/event.rb
+++ app/admin/event.rb
@@ -4,7 +4,7 @@ ActiveAdmin.register Event do
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
-  permit_params :id, :activity_id, :age_min, :age_max, :start_date, :end_date, :start_time, :end_time, :price, :desc
+  permit_params :id, :activity_id, :age_min, :age_max, :start_date, :end_date, :start_time, :end_time, :price, :desc, :frequency
   #
   # or
   #
